9ce9e70afad93425ffac892b82f564866f755934
Luis Henrique M Carvalho <bulinha@gmail.com>
2020-09-17 10:29:37 -0300

    Incluindo configuração para cors

---
+++ src/main/java/br/org/serratec/backend/ecommerce/config/EcommerceWebConfig.java
@@ -0,0 +1,15 @@
+package br.org.serratec.backend.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class EcommerceWebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+
+}
